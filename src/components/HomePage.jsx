1d3a55193d44d661d09185cd2b0e1eb64eb44bee
idkiy0178 <diamant.rubis.78@gmail.com>
2024-05-17 18:16:17 +0900

    updated works and hobbies

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -27,7 +27,7 @@ function HomePage() {
         </div>
 
         <div>
-          <h2 className="xl:text-4xl sm:text-3xl text-2xl font-bold pt-20 pb-5">
+          <h2 className="xl:text-4xl sm:text-3xl text-2xl font-bold pt-28 pb-5">
             Profile
           </h2>
           <table className="text-xl">
@@ -63,7 +63,7 @@ function HomePage() {
         </div>
 
         <div>
-          <h2 className="xl:text-4xl sm:text-3xl text-2xl font-bold pt-20 pb-10">
+          <h2 className="xl:text-4xl sm:text-3xl text-2xl font-bold pt-28 pb-10">
             Works
           </h2>
 
@@ -83,7 +83,7 @@ function HomePage() {
 
                   <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
                     <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-neutral-300 dark:group-hover:text-white">
-                      Studio by Preline
+                      Webサイト制作
                     </h3>
                     <p class="mt-3 text-gray-600 dark:text-neutral-400">
                       Produce professional, reliable streams easily leveraging
@@ -92,7 +92,7 @@ function HomePage() {
                     <p class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium">
                       Read more
                       <svg
-                        class="flex-shrink-0 size-4"
+                        class="flex-shrink-0 size-4 translate-y-0.5 -translate-x-0.5"
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
                         height="24"
@@ -115,23 +115,22 @@ function HomePage() {
                   <div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
                     <img
                       class="group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl"
-                      src="https://images.unsplash.com/photo-1668906093328-99601a1aa584?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80"
+                      src="public\img\signate_student_cup.png"
                       alt="Image Description"
                     />
                   </div>
 
                   <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
                     <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-neutral-300 dark:group-hover:text-white">
-                      Onsite
+                      SIGNATE Student Cup 2023
                     </h3>
                     <p class="mt-3 text-gray-600 dark:text-neutral-400">
-                      Optimize your in-person experience with best-in-class
-                      capabilities like badge printing and lead retrieval
+                      「SIGNATE Student Cup 2023【モデリング部門】:中古車の価格予測チャレンジ」に参加
                     </p>
                     <p class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium">
                       Read more
                       <svg
-                        class="flex-shrink-0 size-4"
+                        class="flex-shrink-0 size-4 translate-y-0.5 -translate-x-0.5"
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
                         height="24"
@@ -161,16 +160,69 @@ function HomePage() {
 
                   <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
                     <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-neutral-300 dark:group-hover:text-white">
-                      The complete guide to OKRs
+                      ハッカソン出場予定
                     </h3>
                     <p class="mt-3 text-gray-600 dark:text-neutral-400">
-                      How to make objectives and key results work for your
-                      company
+                      サポーターズ主催の1週間のハッカソンに出場予定
                     </p>
                     <p class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium">
                       Read more
                       <svg
-                        class="flex-shrink-0 size-4"
+                        class="flex-shrink-0 size-4 translate-y-0.5 -translate-x-0.5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path d="m9 18 6-6-6-6" />
+                      </svg>
+                    </p>
+                  </div>
+                </div>
+              </a>
+
+            </div>
+          </div>
+        </div>
+
+
+
+
+        <div>
+          <h2 className="xl:text-4xl sm:text-3xl text-2xl font-bold pt-28 pb-10">
+            Hobbies
+          </h2>
+          
+          {/* <div class="max-w-[85rem] px-4 py-8 sm:px-6 lg:px-8 lg:py-14 mx-auto"> */}
+          {/* <div class="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto"> */}
+          <div class="max-w-[85rem] mx-auto">
+            <div class="grid grid-cols-1 gap-10">
+              <a class="group rounded-xl overflow-hidden" href="#">
+                <div class="sm:flex">
+                  <div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
+                    <img
+                      class="group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl"
+                      src="https://images.unsplash.com/photo-1540575861501-7cf05a4b125a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                      alt="Image Description"
+                    />
+                  </div>
+
+                  <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
+                    <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-neutral-300 dark:group-hover:text-white">
+                      サッカー
+                    </h3>
+                    <p class="mt-3 text-gray-600 dark:text-neutral-400">
+                      小学校2年生から現在まで15年間サッカーを続けて来ました。
+                    </p>
+                    <p class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium">
+                      Read more
+                      <svg
+                        class="flex-shrink-0 size-4 translate-y-0.5 -translate-x-0.5"
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
                         height="24"
@@ -193,22 +245,23 @@ function HomePage() {
                   <div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
                     <img
                       class="group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl"
-                      src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                      src="https://images.unsplash.com/photo-1668906093328-99601a1aa584?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80"
                       alt="Image Description"
                     />
                   </div>
 
                   <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
                     <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-neutral-300 dark:group-hover:text-white">
-                      People program models
+                      旅行
                     </h3>
                     <p class="mt-3 text-gray-600 dark:text-neutral-400">
-                      Six approaches to bringing your People strategy to life
+                      Optimize your in-person experience with best-in-class
+                      capabilities like badge printing and lead retrieval
                     </p>
                     <p class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium">
                       Read more
                       <svg
-                        class="flex-shrink-0 size-4"
+                        class="flex-shrink-0 size-4 translate-y-0.5 -translate-x-0.5"
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
                         height="24"
@@ -225,15 +278,49 @@ function HomePage() {
                   </div>
                 </div>
               </a>
+
+              <a class="group rounded-xl overflow-hidden" href="#">
+                <div class="sm:flex">
+                  <div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
+                    <img
+                      class="group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl"
+                      src="https://images.unsplash.com/photo-1567016526105-22da7c13161a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+                      alt="Image Description"
+                    />
+                  </div>
+
+                  <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
+                    <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-neutral-300 dark:group-hover:text-white">
+                      料理
+                    </h3>
+                    <p class="mt-3 text-gray-600 dark:text-neutral-400">
+                      How to make objectives and key results work for your
+                      company
+                    </p>
+                    <p class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium">
+                      Read more
+                      <svg
+                        class="flex-shrink-0 size-4 translate-y-0.5 -translate-x-0.5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path d="m9 18 6-6-6-6" />
+                      </svg>
+                    </p>
+                  </div>
+                </div>
+              </a>
+
             </div>
           </div>
-        </div>
 
-        <div>
-          <h2 className="xl:text-4xl sm:text-3xl text-2xl font-bold pt-20 pb-5">
-            Hobby
-          </h2>
-          <p className="text-xl">.....</p>
         </div>
       </div>
     </section>
