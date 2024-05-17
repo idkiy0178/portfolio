731fd1ce8ccd54b67c59a92d1deb6b489601189b
idkiy0178 <diamant.rubis.78@gmail.com>
2024-05-17 11:50:39 +0900

    Worksの内容のテンプレ追加

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,7 @@
 function HomePage() {
   return (
     <section className="text-gray-700" id="home">
-      <div className="md:max-w-[60%] max-w-[70%] mx-auto py-20">
+      <div className="md:max-w-[60%] max-w-[80%] mx-auto py-20">
         <div className="flex md:flex-row flex-col items-center md:justify-between xl:pt-43 sm:pt-31 pt-31 xl:pb-26 pb-24 mx-auto">
           <div className="font-bold whitespace-nowrap md:pb-0 pb-5">
             <h1 class="md:text-left text-center xl:text-3xl md:text-2xl sm:text-2xl text-xl md:pl-1 pl-0">
@@ -63,10 +63,170 @@ function HomePage() {
         </div>
 
         <div>
-          <h2 className="xl:text-4xl sm:text-3xl text-2xl font-bold pt-20 pb-5">
-            Work
+          <h2 className="xl:text-4xl sm:text-3xl text-2xl font-bold pt-20 pb-10">
+            Works
           </h2>
-          <p className="text-xl">.....</p>
+
+          {/* <div class="max-w-[85rem] px-4 py-8 sm:px-6 lg:px-8 lg:py-14 mx-auto"> */}
+          {/* <div class="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto"> */}
+          <div class="max-w-[85rem] mx-auto">
+            <div class="grid grid-cols-1 gap-10">
+              <a class="group rounded-xl overflow-hidden" href="#">
+                <div class="sm:flex">
+                  <div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
+                    <img
+                      class="group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl"
+                      src="https://images.unsplash.com/photo-1540575861501-7cf05a4b125a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                      alt="Image Description"
+                    />
+                  </div>
+
+                  <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
+                    <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-neutral-300 dark:group-hover:text-white">
+                      Studio by Preline
+                    </h3>
+                    <p class="mt-3 text-gray-600 dark:text-neutral-400">
+                      Produce professional, reliable streams easily leveraging
+                      Preline's innovative broadcast studio
+                    </p>
+                    <p class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium">
+                      Read more
+                      <svg
+                        class="flex-shrink-0 size-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path d="m9 18 6-6-6-6" />
+                      </svg>
+                    </p>
+                  </div>
+                </div>
+              </a>
+
+              <a class="group rounded-xl overflow-hidden" href="#">
+                <div class="sm:flex">
+                  <div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
+                    <img
+                      class="group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl"
+                      src="https://images.unsplash.com/photo-1668906093328-99601a1aa584?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80"
+                      alt="Image Description"
+                    />
+                  </div>
+
+                  <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
+                    <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-neutral-300 dark:group-hover:text-white">
+                      Onsite
+                    </h3>
+                    <p class="mt-3 text-gray-600 dark:text-neutral-400">
+                      Optimize your in-person experience with best-in-class
+                      capabilities like badge printing and lead retrieval
+                    </p>
+                    <p class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium">
+                      Read more
+                      <svg
+                        class="flex-shrink-0 size-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path d="m9 18 6-6-6-6" />
+                      </svg>
+                    </p>
+                  </div>
+                </div>
+              </a>
+
+              <a class="group rounded-xl overflow-hidden" href="#">
+                <div class="sm:flex">
+                  <div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
+                    <img
+                      class="group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl"
+                      src="https://images.unsplash.com/photo-1567016526105-22da7c13161a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+                      alt="Image Description"
+                    />
+                  </div>
+
+                  <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
+                    <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-neutral-300 dark:group-hover:text-white">
+                      The complete guide to OKRs
+                    </h3>
+                    <p class="mt-3 text-gray-600 dark:text-neutral-400">
+                      How to make objectives and key results work for your
+                      company
+                    </p>
+                    <p class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium">
+                      Read more
+                      <svg
+                        class="flex-shrink-0 size-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path d="m9 18 6-6-6-6" />
+                      </svg>
+                    </p>
+                  </div>
+                </div>
+              </a>
+
+              <a class="group rounded-xl overflow-hidden" href="#">
+                <div class="sm:flex">
+                  <div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
+                    <img
+                      class="group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl"
+                      src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                      alt="Image Description"
+                    />
+                  </div>
+
+                  <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
+                    <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-neutral-300 dark:group-hover:text-white">
+                      People program models
+                    </h3>
+                    <p class="mt-3 text-gray-600 dark:text-neutral-400">
+                      Six approaches to bringing your People strategy to life
+                    </p>
+                    <p class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium">
+                      Read more
+                      <svg
+                        class="flex-shrink-0 size-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path d="m9 18 6-6-6-6" />
+                      </svg>
+                    </p>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
         </div>
 
         <div>
